6b7020890d3b91816792cbf72974b43ba4064685
MichaelSlice <mjsens523@gmail.com>
2025-01-31 17:34:38 -0600

    Added Detectron2 build

---
+++ Examples/Detectron2_Test/Detector.py
@@ -0,0 +1,5 @@
+from detectron2.detectron2.engine import DefaultPredictor
+from detectron2.detectron2.config import get_cfg
+from detectron2.detectron2.data import MetadataCatalog
+from detectron2.detectron2.utils.visualizer import ColorMode, Visualizer 
+
