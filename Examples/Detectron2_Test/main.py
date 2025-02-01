a4e2df4114f39cebdd06360a5c4e184ddb2558fc
MichaelSlice <mjsens523@gmail.com>
2025-01-31 18:04:38 -0600

    Added detection example

---
+++ Examples/Detectron2_Test/main.py
@@ -0,0 +1,7 @@
+from Detector import *
+
+detector = Detector()
+
+# For some reason it only works with absolute path on windows...
+detector.onImage("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/images/1.jpg")
+
