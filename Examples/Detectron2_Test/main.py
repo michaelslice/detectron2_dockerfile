0f65e8bd5ec00832fabaeab9c394c39b6b6f2305
MichaelSlice <mjsens523@gmail.com>
2025-01-31 21:18:53 -0600

    Added person script

--- Examples/Detectron2_Test/main.py
+++ Examples/Detectron2_Test/main.py
@@ -1,7 +1,13 @@
 from Detector import *
 
-detector = Detector()
-
+# OD
+# IS
+# KP, pose mapping
+# LVIS, trained on more rare item dataset
+# PS
+detector = Detector(model_type="PS")
 # For some reason it only works with absolute path on windows...
-detector.onImage("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/images/1.jpg")
-
+# detector.onImage("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/data/1.jpg")
+# detector.onImage("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/data/2.jpg")
+# detector.onVideo("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/data/3.MOV")
+detector.onVideo("C:/Users/User/Desktop/igait-video-precheck/Examples/Detectron2_Test/data/5.MOV")
